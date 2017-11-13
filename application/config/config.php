39c6dd0fe45d2ba4c17090b2aac1f56296be043d
monosolo159 <wichetpong159@hotmail.com>
2017-11-13 18:17:02 +0700

    delete index.php link

--- application/config/config.php
+++ application/config/config.php
@@ -40,7 +40,7 @@ $config['base_url'] = 'http://localhost/ritipon/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
